c2757b4d35b3ba01f40991e2e43c8654e2c0f18c
IamEmpty <dustonroad@gmail.com>
2015-04-18 13:33:44 +0300

    remove output file

--- app/server.js
+++ app/server.js
@@ -7,47 +7,28 @@ var app     = express();
 
 app.get('/scrape', function( req, res ) {
     
-    url = 'http://www.imdb.com/title/tt1229340/';
+    url = 'http://www.apache.org/';
 
-    request(url, function( error, response, html ) {
+    request( url, function( error, response, html ) {
         if( !error ) {
             var $ = cheerio.load(html);
 
             var title, release, rating;
             var json = { title : "", release : "", rating : ""};
 
-            $('.header').filter(function(){
+            $('.panel-heading').filter(function() {
                 var data = $(this);
-                title = data.children().first().text();
-            
-                release = data.children().last().children().text();
-
+                title = data.find('.panel-title').text();
                 json.title = title;
-                json.release = release;
-            })
-
-            // Since the rating is in a different section of the DOM, we'll have to write a new jQuery filter to extract this information.
-
-            $('.star-box-giga-star').filter(function(){
-                var data = $(this);
-
-                // The .star-box-giga-star class was exactly where we wanted it to be.
-                // To get the rating, we can simply just get the .text(), no need to traverse the DOM any further
-
-                rating = data.text();
-
-                json.rating = rating;
             })
         }
 
-        fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-
-		    console.log('File successfully written! - Check your project directory for the output.json file');
-
+        fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
+		    console.log( 'File successfully written! - Check your project directory for the output.json file' );
 		});
 
 		// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
-		res.send('Check your console!');
+		res.send( 'Check your console!' );
     });
 });
 
