98b70d90eabf1ab8ad0f09689249d6686216f725
IamEmpty <dustonroad@gmail.com>
2015-07-28 14:36:03 +0300

    add opportunity to get html from web page

--- phantom.js
+++ phantom.js
@@ -6,12 +6,10 @@ page.open('http://yed0ob.axshare.com/', function(status) {
   console.log("Status: " + status);
 
   if(status === "success") {
-    page.render('example.png');
+    var path = 'output.html';
+    // Write html to output.html file
+    fs.write(path, page.content, 'w');
   }
-
-  var path = 'output.html';
-  // Write html to output.html file
-  fs.write(path, '<h1>Heading 1</h1>', 'w');
 
   phantom.exit();
 });
